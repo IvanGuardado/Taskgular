918e4e72998ed18e3ea2752ce85473007a795f52
Iván Guardado <contact@ivanguardado.com>
2013-05-29 10:36:49 +0200

    local CRUD

--- app/main.js
+++ app/main.js
@@ -4,12 +4,29 @@ sb.controller('MainCtrl', function($scope){
 
 });
 
-sb.controller('AddCtrl', function($scope){
+sb.controller('AddCtrl', function($scope, tasks){
   $scope.showForm = function(){
     $scope.formVisible = true;
   };
   
   $scope.hideForm = function(){
+    $scope.desc = '';
     $scope.formVisible = false;
   };
+
+  $scope.add = function(){
+    tasks.push({desc: $scope.desc});
+    $scope.hideForm();
+  }
 });
+
+sb.controller('ListCtrl', function($scope, tasks){
+  $scope.tasks = tasks;
+  $scope.remove = function(index){
+    tasks.splice(index,1);
+  }
+});
+
+sb.factory('tasks', function(){
+  return [{desc:'Esto es una tarea'}, {desc: 'Y otra tarea'}];
+})
