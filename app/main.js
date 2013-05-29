f71a22b023feb6b62fd6d625475d687601ded2b8
Iván Guardado <contact@ivanguardado.com>
2013-05-29 10:18:48 +0200

    toggle new task form

--- app/main.js
+++ app/main.js
@@ -3,3 +3,13 @@ var sb = angular.module('sb', []);
 sb.controller('MainCtrl', function($scope){
 
 });
+
+sb.controller('AddCtrl', function($scope){
+  $scope.showForm = function(){
+    $scope.formVisible = true;
+  };
+  
+  $scope.hideForm = function(){
+    $scope.formVisible = false;
+  };
+});
